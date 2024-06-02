f39cd1336afeca04b48831b6e39707e4b8a10ec3
Kamil Kubiczek <lukuku001@gmail.com>
2024-06-02 11:10:12 +0200

    feat: stack

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,26 +11,39 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
+        Ticket::validate_title(&title);
+        Ticket::validate_description(&description);
+        Ticket::validate_status(&status);
+
+        Ticket {
+            title,
+            description,
+            status,
+        }
+    }
+
+    fn validate_title(title: &String) {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
+
         if title.len() > 50 {
             panic!("Title cannot be longer than 50 bytes");
         }
+    }
+
+    fn validate_description(description: &String) {
         if description.is_empty() {
             panic!("Description cannot be empty");
         }
         if description.len() > 500 {
             panic!("Description cannot be longer than 500 bytes");
         }
+    }
+
+    fn validate_status(status: &String) {
         if status != "To-Do" && status != "In Progress" && status != "Done" {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
-
-        Ticket {
-            title,
-            description,
-            status,
         }
     }
 
@@ -44,6 +57,21 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, new_title: String) {
+        Ticket::validate_title(&new_title);
+        self.title = new_title
+    }
+
+    pub fn set_description(&mut self, new_desc: String) {
+        Ticket::validate_description(&new_desc);
+        self.description = new_desc
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        Ticket::validate_status(&new_status);
+        self.status = new_status
     }
 }
 
